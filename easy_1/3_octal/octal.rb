7e7de3d70d5612e5450b6a3602fdb3a90e926c2b
Balal Naeem <balalnaeem@gmail.com>
2019-07-04 07:27:38 +0100

    octal solution

---
+++ easy_1/3_octal/octal.rb
@@ -0,0 +1,27 @@
+=begin
+  Implement octal to decimal conversion. Given an octal input string, your program should produce a decimal output.
+
+  Treat invalid input as octal 0
+
+  In: octal number (string)
+  Out: decimal (integer)
+
+  Al:
+  - if from string input, any digit is greater than 7, return 0
+  - best pseudocode is written with hand
+=end
+
+class Octal
+  def initialize(octal_string)
+    @octal_string = octal_string
+    @digits = octal_string.chars.map(&:to_i)
+  end
+
+  def to_decimal
+    return 0 if @octal_string.match?(/[a-z]/i) || @digits.any? { |n| n > 7 }
+    @digits.reverse.map.with_index do |num, idx|
+      num * (8**idx)
+    end.inject(:+)
+  end
+end
+
